307107919d0c74a9092ac3ea98e14ddcaa488f73
David Guijarro <guijarro.dav@gmail.com>
2018-11-18 16:11:59 +0100

    Initial commit

---
+++ src/components/date/date.tsx
@@ -0,0 +1,16 @@
+import { Component } from '@stencil/core';
+
+@Component({
+  tag: 'adc-date',
+  // styleUrl: 'my-component.css',
+  shadow: true,
+})
+export class Date {
+  // @Prop() first: string;
+  // @Prop() middle: string;
+  // @Prop() last: string;
+
+  render() {
+    return <div>Oh hello there</div>;
+  }
+}
